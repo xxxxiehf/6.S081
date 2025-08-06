bff3238f1f3bef5ca2fbd16b96d99abf6131096c
Haofeng Xie <xxxxiehf@gmail.com>
2025-08-05 19:49:16 -0700

    Format everything.

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,13 +1,12 @@
 // Mutual exclusion lock.
 struct spinlock {
-  uint locked;       // Is the lock held?
+    uint locked; // Is the lock held?
 
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
+    // For debugging:
+    char *name;      // Name of lock.
+    struct cpu *cpu; // The cpu holding the lock.
 #ifdef LAB_LOCK
-  int nts;
-  int n;
+    int nts;
+    int n;
 #endif
 };
-
