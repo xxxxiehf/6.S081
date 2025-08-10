ef0c880d1e42c66a8dee808e16ac9896e4f76506
Haofeng Xie <xxxxiehf@gmail.com>
2025-08-10 00:12:32 -0700

    Finish `memory allocator`.

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -11,8 +11,8 @@
 
 void freerange(void *pa_start, void *pa_end);
 
-extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+extern char end[];  // first address after kernel.
+                    // defined by kernel.ld.
 
 struct run {
     struct run *next;
@@ -21,10 +21,16 @@ struct run {
 struct {
     struct spinlock lock;
     struct run *freelist;
-} kmem;
+    uint8 waiting;
+} kmem[NCPU];
 
 void kinit() {
-    initlock(&kmem.lock, "kmem");
+    for (int i = 0; i < NCPU; i++) {
+        char *name = "kmem-0";
+        name[5] += i;
+        initlock(&kmem[i].lock, name);
+        kmem[i].waiting = 0;
+    }
     freerange(end, (void *)PHYSTOP);
 }
 
@@ -50,25 +56,61 @@ void kfree(void *pa) {
 
     r = (struct run *)pa;
 
-    acquire(&kmem.lock);
-    r->next = kmem.freelist;
-    kmem.freelist = r;
-    release(&kmem.lock);
+    // disable interrputs to get the CPU ID
+    push_off();
+    int cpu_id = cpuid();
+    pop_off();
+
+    acquire(&kmem[cpu_id].lock);
+    r->next = kmem[cpu_id].freelist;
+    kmem[cpu_id].freelist = r;
+    release(&kmem[cpu_id].lock);
+}
+
+void *kalloc_cpu(int cpu_id) {
+    if (kmem[cpu_id].waiting == 1) {
+        return 0;
+    }
+    
+    struct run *r;
+    
+    acquire(&kmem[cpu_id].lock);
+    r = kmem[cpu_id].freelist;
+    if (r) {
+        kmem[cpu_id].freelist = r->next;
+        release(&kmem[cpu_id].lock);
+        return r;
+    }
+    kmem[cpu_id].waiting = 1;
+    release(&kmem[cpu_id].lock);
+    for (int i = 0; i < NCPU; i++) {
+        if (i == cpu_id) {
+            continue;
+        }
+        r = kalloc_cpu(i);
+        if (r) {
+            break;
+        }
+    }
+    kmem[cpu_id].waiting = 0;
+
+    return r;
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *kalloc(void) {
-    struct run *r;
+    // disable interrputs to get the CPU ID
+    push_off();
+    int cpu_id = cpuid();
+    pop_off();
 
-    acquire(&kmem.lock);
-    r = kmem.freelist;
-    if (r)
-        kmem.freelist = r->next;
-    release(&kmem.lock);
+    void *r = kalloc_cpu(cpu_id);
 
-    if (r)
-        memset((char *)r, 5, PGSIZE); // fill with junk
-    return (void *)r;
+    if (r) {
+        memset((char *)r, 5, PGSIZE);
+    }
+
+    return r;
 }
